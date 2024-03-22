11413f770f3f50d014b15b42a6cc83e4df535c74
Khadiev Edem <khadiev.edem@gmail.com>
2024-03-22 12:28:52 +0300

    Test all functions from connected components

--- UnitTestFiles/SourceFiles/EcoLab2.c
+++ UnitTestFiles/SourceFiles/EcoLab2.c
@@ -20,10 +20,12 @@
 
 /* Eco OS */
 #include "IEcoSystem1.h"
+#include "IdEcoLab2.h"
 #include "IdEcoMemoryManager1.h"
 #include "IdEcoInterfaceBus1.h"
 #include "IdEcoFileSystemManagement1.h"
-#include "IdEcoLab2.h"
+#include "IdEcoLab1.h"
+#include "IdEcoComplex1.h"
 #include "IEcoCalculatorX.h"
 #include "IEcoCalculatorY.h"
 #include "IdEcoCalculatorC.h"
@@ -41,7 +43,7 @@
  */
 int16_t EcoMain(IEcoUnknown* pIUnk) {
     int16_t result = -1;
-    int32_t a, b;
+    int32_t a, b, N, i;
     /* Указатель на системный интерфейс */
     IEcoSystem1* pISys = 0;
     /* Указатель на интерфейс работы с системной интерфейсной шиной */
@@ -50,6 +52,11 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     IEcoMemoryAllocator1* pIMem = 0;
     /* Указатель на тестируемый интерфейс */
     IEcoLab2* pIEcoLab2 = 0;
+
+    /* Source vector for dft/fft counting */
+    int32_t   *v_src = 0;
+    /* Result vector for dft/fft result */
+    complex_t *v_res = 0;
 
     /* Проверка и создание системного интрефейса */
     if (pISys == 0) {
@@ -80,6 +87,21 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
         goto Release;
     }
 
+    /* Регистрация статического компонента для работы с комплексными числами */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoComplex1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_1F5DF16EE1BF43B999A434ED38FFFFFF);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+    
+    /* Регистрация статического компонента для работы со списком */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoLab1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_1F5DF16EE1BF43B999A434ED38FE8F3A);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+
+
 #endif
     /* Получение интерфейса управления памятью */
     result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoMemoryManager1, 0, &IID_IEcoMemoryAllocator1, (void**) &pIMem);
@@ -91,6 +113,9 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     }
 
     /* Выделение блока памяти */
+    N = 8;
+    v_src = (int32_t *)pIMem->pVTbl->Alloc(pIMem, N * sizeof(int32_t));
+    v_res = (complex_t *)pIMem->pVTbl->Alloc(pIMem, N * sizeof(complex_t));
 
 
     /* Получение тестируемого интерфейса */
@@ -106,11 +131,31 @@ int16_t EcoMain(IEcoUnknown* pIUnk) {
     fprintf(stdout, "%d - %d = %d\n", a, b, pIEcoLab2->pVTbl->Subtraction(pIEcoLab2, a, b));
     fprintf(stdout, "%d * %d = %d\n", a, b, pIEcoLab2->pVTbl->Multiplication(pIEcoLab2, a, b));
     fprintf(stdout, "%d / %d = %d\n", a, b, pIEcoLab2->pVTbl->Division(pIEcoLab2, a, b));
-    /* pIEcoLab2->pVTbl->fft(); */
 
+    v_src[0] = 1;
+    v_src[1] = -1;
+    v_src[2] = 1;
+    v_src[3] = -1;
+    v_src[4] = 5;
+    v_src[5] = 4;
+    v_src[6] = 3;
+    v_src[7] = 2;
+    
+    fprintf(stdout, "Source array for fft:\n");
+    for (i = 0; i < N; ++i) {
+        fprintf(stdout, "%d\n", v_src[i]);
+    }
 
+    pIEcoLab2->pVTbl->fft(pIEcoLab2, 1, N, v_src, v_res);
+
+    fprintf(stdout, "Result fft array:\n");
+    for (i = 0; i < N; ++i) {
+        fprintf(stdout, "%.4f + %.4f * i\n", v_res[i].re, v_res[i].im);
+    }
 
     /* Освлбождение блока памяти */
+    pIMem->pVTbl->Free(pIMem, v_src);
+    pIMem->pVTbl->Free(pIMem, v_res);
 
 Release:
 
